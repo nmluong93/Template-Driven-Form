e806b748b936c23b3d5c9e78aac00971264a8823
luongnm <luong.nguyen@adnovum.vn>
2021-03-31 09:30:10 +0700

    add tutorial link and style

--- src/app/hero-form/hero-form.component.ts
+++ src/app/hero-form/hero-form.component.ts
@@ -18,9 +18,6 @@ export class HeroFormComponent {
 
   onSubmit() { this.submitted = true; }
 
-  // TODO: Remove this when we're done
-  get diagnostic() { return JSON.stringify(this.model); }
-
   newHero() {
     this.model = new Hero(42, '', '');
   }
